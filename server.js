d7c805b4160fa23b47346827c5d757ed47b79bdb
Mark <mrbruce9866@gmail.com>
2019-08-22 17:21:36 -0400

    dotenv

--- server.js
+++ server.js
@@ -11,12 +11,10 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gamescraper"
 var PORT = process.env.PORT ||3000;
 
 var app = express();
-app.use(express.static('models'))
+app.use(express.static('public'))
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/public", express.static(path.join(__dirname + "/public")));
-app.use("/models", express.static(path.join(__dirname + "/models")));
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
 app.engine("handlebars",exphbs({defaultLayout:"main"}))
