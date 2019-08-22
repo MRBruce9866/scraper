2e7b50305a5992273cdb232a9554c8711891f754
Jester <ABooth9422@gmail.com>
2019-08-21 21:40:01 -0400

    another attempt for heroku

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gamescraper"
 var PORT = process.env.PORT ||3000;
 
 var app = express();
-
+app.use(express.static('models'))
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname + "/public")));
-app.use("/models", express.static(path.join(__dirname + "/models")));
+
 
 app.engine("handlebars",exphbs({defaultLayout:"main"}))
 app.set("view engine","handlebars")
