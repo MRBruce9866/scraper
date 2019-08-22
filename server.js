405ab7fad7597a1bd7d7d7e01ffafad2bcf1c44f
Mark <mrbruce9866@gmail.com>
2019-08-22 17:43:50 -0400

    added

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ var PORT = process.env.PORT ||3000;
 var app = express();
 app.use(express.static('public'))
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use("/public", express.static(path.join(__dirname + "/public")));
-app.use("/models", express.static(path.join(__dirname + "/models")));
+// app.use("/models", express.static(path.join(__dirname + "/models")));
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
 app.engine("handlebars",exphbs({defaultLayout:"main"}))
